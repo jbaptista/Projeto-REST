dc0f01bddaa8fcabd9a848ba6cb9ede4018b14d7
Juan <juanctb@gmail.com>
2020-01-23 15:58:55 -0300

    Adicionando handler para simplificação de respostas de bad request

---
+++ src/main/java/br/com/juanbaptista/mybooks/config/validacao/ErroDeFormularioDto.java
@@ -0,0 +1,26 @@
+package br.com.juanbaptista.mybooks.config.validacao;
+
+public class ErroDeFormularioDto {
+	
+	private String campo;
+	private String erro;
+	
+	public ErroDeFormularioDto(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+	public String getCampo() {
+		return campo;
+	}
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	public String getErro() {
+		return erro;
+	}
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+	
+
+}
